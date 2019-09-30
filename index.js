1793873a7280270edd6f80353bd66199db4589b1
Uriah Sanders <uriahsanders@bitbucket.org>
2019-09-29 17:50:39 -0700

    Added almost complete book of sasame, added some design improvements, a light counter, and made the seed a little bigger, as is appropriate

--- index.js
+++ index.js
@@ -20,19 +20,30 @@ mongoose.connect('mongodb://localhost/sasame', { useNewUrlParser: true }, functi
         mongoose.connect('mongodb://localhost/sasame', options);
     }, 5000);
 });
+var categorySchema = mongoose.Schema({
+    author: String,
+    title: String, //name of the category
+    passages: [Number], //array of passage IDs
+});
 var passageSchema = mongoose.Schema({
     author: String,
     rank: Number, //rank is the average of the array
-    content: String
+    content: String,
 });
 var Passage = mongoose.model('Post', passageSchema, 'Posts');
+var Category = mongoose.model('Category', passageSchema, 'Categories');
 
 app.get('/', function(req, res) {
-    Passage.findOne().sort({_id: -1}).exec(function(err, passage) {
-        if(!err) {
-            res.render('index', { passage: passage });
-        }
-        console.log(err);
+    Passage.count({}, function( err, count){
+        console.log(count);
+        Passage.findOne().sort({_id: -1}).exec(function(err, passage) {
+            if(!err) {
+                res.render('index', { passage: passage, light: count });
+            }
+            else{
+                console.log(err);
+            }
+        });
     });
     // Passage.find({}, (err, passages) => {
     //     if(!err) {
@@ -44,8 +55,8 @@ app.get('/', function(req, res) {
 app.get('/about', function(req, res) {
     res.render('about', { posts });
 });
-app.get('/blog', function(req, res) {
-    res.render('blog', { posts });
+app.get('/history', function(req, res) {
+    res.render('history', { posts });
 });
 app.get('/team', function(req, res) {
     res.render('blog', { posts });
@@ -53,9 +64,9 @@ app.get('/team', function(req, res) {
 app.get('/applications', function(req, res) {
     res.render('blog', { posts });
 });
-app.get('/control', function(req, res) {
+app.get('/roots', function(req, res) {
     Passage.find().sort([['_id', 1]]).exec(function(err, response){
-        res.render("control", {book: response});
+        res.render("roots", {book: response});
     });
 });
 var add_passage = function(content, callback) {
@@ -69,6 +80,27 @@ var add_passage = function(content, callback) {
         console.log(err);
     });
 };
+var add_category = function(title, callback) {
+    let category = new Category({
+        title: title
+    });
+    category.save().then((data) => {
+        callback();
+    }).catch((err) => {
+        console.log(err);
+    });
+};
+var add_passage_to_category = function(passageID, categoryID, callback) {
+    Category.find({_id:categoryID}).sort([['_id', 1]]).exec(function(err, category){
+        category.passages.append(passageID);
+        category.save().then((data) => {
+            callback();
+        });
+    });
+};
+var get_density_of_category = function(category){
+    return category.passages.length;
+};
 
 app.get('/feed_sasame', (req, res) => {
     let info = req.query;
@@ -78,10 +110,13 @@ app.get('/feed_sasame', (req, res) => {
     Passage.findOne().sort({_id: -1}).exec(function(err, passage) {
         if (info.content != passage.content){
             add_passage(info.content, function(){
-                res.redirect("/fruit");
+                res.redirect("/");
             });
         }
         else{
+            add_passage('Light', function(){
+                res.redirect("/");
+            });
             res.redirect("/");
         }
     });
@@ -156,3 +191,47 @@ app.listen(3000, () => {
 
 
 //
+// * Someone tweets “I hate Donald Trump”
+// * For each groups of words in tweet, measure density of its category by num passages/categories within it
+// * Choose the densest group
+// * In this case ‘Donald Trump’
+// * ‘Donald Trump’ category will have every passage ever written about Donald Trump
+// * Iterate over passages
+// * check if passage is also in the ‘I’ category and ‘hate’ category and every other category in the string
+// * choose the passage that fits all first, or fits the most by the end.
+// * This should produce an agreeable statement.
+
+// Infinite Sieve
+// This function should be constantly expanded
+// in order to fractally categorize data.
+// But for now we will anchor on the densest word
+// and then find passages in that category that also match the other words
+var infinite_sieve = function(post){
+    //iterate over every group of words in post
+    //later we will need to clean all but single spaces and 'pure words'
+    words = post.split(' ');
+    num_words = words.length;
+    //we could do more but for now simply use the word
+    //which has the densest category
+    //as an anchor
+    var density = 0;
+    var anchor = null;
+    for(var i = 0; i < num_words; ++i){
+        temp = getDensityOfCategory(words[i]);
+        if(temp > density){
+            density = temp;
+            anchor = words[i];
+        }
+
+    }
+    // Now get all the passages in the category 'anchor'
+
+    //iterate through all passages
+    Passage.find().sort([['_id', 1]]).exec(function(err, passages){
+        for(var i = 0; i < passages.length; ++i){
+            content = passages[i].content;
+        }
+    });
+
+
+};
