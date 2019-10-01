3982424963d78f4329300c5e87fd90b5114c2421
Uriah Sanders <uriahsanders@bitbucket.org>
2019-09-30 19:02:29 -0700

    control panel v1 is done but needs mobile, sequential ordering, and up one level to work better

--- index.js
+++ index.js
@@ -23,15 +23,34 @@ mongoose.connect('mongodb://localhost/sasame', { useNewUrlParser: true }, functi
 var categorySchema = mongoose.Schema({
     author: String,
     title: String, //name of the category
-    passages: [Number], //array of passage IDs
+    //passages that belong to this category
+    passages: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Passage'
+    }], //array of passage IDs
+    //categories that belong to this category
+    categories: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
+    }],
+    //category the category belongs to
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
+    }
 });
 var passageSchema = mongoose.Schema({
     author: String,
     rank: Number, //rank is the average of the array
     content: String,
+    //category the passage belongs to
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
+    }
 });
 var Passage = mongoose.model('Post', passageSchema, 'Posts');
-var Category = mongoose.model('Category', passageSchema, 'Categories');
+var Category = mongoose.model('Category', categorySchema, 'Categories');
 
 app.get('/', function(req, res) {
     Passage.count({}, function( err, count){
@@ -64,31 +83,89 @@ app.get('/team', function(req, res) {
 app.get('/applications', function(req, res) {
     res.render('blog', { posts });
 });
-app.get('/roots', function(req, res) {
-    Passage.find().sort([['_id', -1]]).exec(function(err, response){
-        res.render("roots", {book: response});
-    });
+app.get(/\/sasasame\/?(:category\/:category_ID)?/, function(req, res) {
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    var url_end = fullUrl.split('/')[fullUrl.split('/').length - 1];
+    //home page
+    if(url_end == '' || url_end.length < 15){
+        Category.find().sort({_id: 1}).exec(function(err, categories){
+            Passage.find().sort([['_id', 1]]).exec(function(err, passages){
+                res.render("sasasame", {category: '', book: passages, categories: categories});
+            });
+        });
+    }
+    //category ID
+    else{
+        //find all passages in this category
+        Passage.find({category: url_end}).exec(function(err, passages){
+            //find all categories in this category
+            Category.find({category: url_end}).exec(function(err, cats){
+                res.render("sasasame", {category: url_end, book: passages, categories: cats});
+            });
+        });
+        // Category.findOne({_id:url_end}).exec(function(err, category){
+        //     console.log(category.passages);
+        //     if(!category.passages){
+        //         category.passages = '';
+        //     }
+        //     if(!category.categories){
+        //         category.categories = '';
+        //     }
+        //     res.render("sasasame", {category: url_end, book: category.passages, categories: category.categories});
+        // });
+    }
 });
-var add_passage = function(content, callback) {
-    let post = new Passage({
-        content: content
-    });
-    post.save().then((data) => {
-        console.log("inserted", data.content);
-        callback();
-    }).catch((err) => {
-        console.log(err);
-    });
+var add_passage = function(category, content, callback) {
+    if(category != ''){
+        let post = new Passage({
+            content: content,
+            category: category
+        }).save().then(data => {
+            if(category != ''){
+                Category.findOne({_id:category}).exec(function(err, cat){
+                    if(cat.passages){
+                        cat.passages.push(data);
+                    }
+                    else{
+                        cat.passages = [data];
+                    }
+                    cat.save();
+                });
+            }
+        });
+    }
+    else{
+        let post = new Passage({
+            content: content,
+        }).save();
+    }
+    callback();
 };
-var add_category = function(title, callback) {
-    let category = new Category({
-        title: title
-    });
-    category.save().then((data) => {
-        callback();
-    }).catch((err) => {
-        console.log(err);
-    });
+var add_category = function(cat, title, callback) {
+    if(cat != ''){
+        let category = new Category({
+            title: title,
+            category: cat
+        }).save().then(data => {
+            if(cat != ''){
+                Category.findOne({_id:cat}).exec(function(err, cat){
+                    if(cat.categories){
+                        cat.categories.push(data);
+                    }
+                    else{
+                        cat.categories = [data];
+                    }
+                    cat.save();
+                });
+            }
+        });
+    }
+    else{
+        let category = new Category({
+            title: title,
+        }).save();
+    }
+    callback();
 };
 var add_passage_to_category = function(passageID, categoryID, callback) {
     Category.find({_id:categoryID}).sort([['_id', 1]]).exec(function(err, category){
@@ -102,6 +179,32 @@ var get_density_of_category = function(category){
     return category.passages.length;
 };
 
+app.get(/\/add_category\/?(:categoryID)?/, (req, res) => {
+    let info = req.query;
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    var url_end = fullUrl.split('/')[fullUrl.split('/').length - 1];
+    var categoryID = url_end.split('?')[0] || '';
+    var backURL=req.header('Referer') || '/';
+    if(info.passage != ''){
+        add_category(categoryID, info.title, function(){
+            res.redirect(backURL);
+        });
+    }
+    res.redirect(backURL);
+});
+app.get(/\/add_passage\/?(:categoryID)?/, (req, res) => {
+    let info = req.query;
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    var url_end = fullUrl.split('/')[fullUrl.split('/').length - 1];
+    var categoryID = url_end.split('?')[0] || '';
+    var backURL=req.header('Referer') || '/';
+    if(info.passage != ''){
+        add_passage(categoryID, info.passage, function(){
+            res.redirect(backURL);
+        });
+    }
+    res.redirect(backURL);
+});
 app.get('/feed_sasame', (req, res) => {
     let info = req.query;
     // author,
@@ -114,7 +217,7 @@ app.get('/feed_sasame', (req, res) => {
             });
         }
         else{
-            add_passage('Light', function(){
+            add_passage('', 'Light', function(){
                 res.redirect("/");
             });
             res.redirect("/");
