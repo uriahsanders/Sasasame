810e2fdc3957c0cb4b2f53cf8ad6a779aeceb509
Perfect 7 <uriahrsanders@gmail.com>
2020-02-25 23:19:01 -0800

    Styling finally updated for mobile!

--- dist/js/book.js
+++ dist/js/book.js
@@ -417,14 +417,6 @@ $('#parent_chapter_title').on('click', function(){
     }
 
 });
-$('#mobile_active_close').on('click', function(){
-    $('#mobile_book_menu_main').fadeOut();
-    $('#control_panel').fadeIn();
-});
-$('#mobile_book_menu').on('click', function(){
-    $('#control_panel').fadeOut();
-    $('#mobile_book_menu_main').css('display', 'block');
-});
 $('.category').on('mouseover', function(){
     $(this).find('.chapter_flag').show();
 });
